3051e9044ebb456dfd972c918554fdca339ffeb8
webkawsar <web.kawsarahmed@gmail.com>
2022-11-08 18:29:12 +0600

    added cloudinary and mailgun

---
+++ config/plugins.js
@@ -0,0 +1,32 @@
+module.exports = ({ env }) => ({
+    email: {
+      config: {
+        provider: 'mailgun',
+        providerOptions: {
+          key: env('MAILGUN_API_KEY'), // Required
+          domain: env('MAILGUN_DOMAIN'), // Required
+          url: env('MAILGUN_URL', 'https://api.mailgun.net'), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+        },
+        settings: {
+          defaultFrom: 'myemail@protonmail.com',
+          defaultReplyTo: 'myemail@protonmail.com',
+        },
+      },
+    },
+    upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
+    
+  });
