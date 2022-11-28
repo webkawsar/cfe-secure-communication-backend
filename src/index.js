eff55f3068dd13e288b39d88af28e3b6b0b1acea
webkawsar <web.kawsarahmed@gmail.com>
2022-11-28 23:57:14 +0600

    added socket

--- src/index.js
+++ src/index.js
@@ -16,5 +16,41 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+
+    // const express = require('express')
+    // const app = express();
+    // const http = require('http');
+    // const expressServer = http.createServer(app);
+
+    // const {Server} = require('socket.io');
+    // const io = new Server(expressServer);
+
+    // io.on('connection', (socket) => {
+    //   console.log('New User Connected');
+      
+    //   socket.on('disconnect', () => {
+        
+    //     console.log('New User Disconnect');
+    //   })
+    // })
+
+
+    const io = require('socket.io')(strapi.server.httpServer, {
+      path: '/api/socket/v1',
+      cors: {origin: "*", methods: ['GET', 'POST', 'PUT', 'DELETE']},
+      credential: true
+    })
+
+    strapi.io = io
+
+    io.on('connection', (socket) => {
+      console.log('Socket.io Connected')
+
+      socket.on('disconnect', () => {
+        console.log('Socket.io Disconnect');
+      })
+    })
+
+  },
 };
