97545f0f9418073548d2a5390efcc3ffa36da992
webkawsar <web.kawsarahmed@gmail.com>
2022-12-05 18:16:21 +0600

    modify /message route findone controller

--- src/api/message/controllers/message.js
+++ src/api/message/controllers/message.js
@@ -6,4 +6,29 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::message.message');
+module.exports = createCoreController('api::message.message', ({strapi}) => {
+    return {
+        async findOne(ctx) {
+
+            try {
+
+                const {id} = ctx.params;
+                const {user:{username}} = ctx.state;
+                const messages = await strapi.entityService.findMany('api::message.message', {
+                    populate: '*',
+                });
+
+                const filteredMessages = messages.filter(m => {
+
+                    return (m?.sender?.username === username && m?.receiver?.username === id) || 
+                    (m?.sender?.username === id && m?.receiver?.username === username)
+                });
+
+                return filteredMessages;
+                
+            } catch (error) {
+                ctx.internalServerError('Internal Server Error');
+            }
+        }
+    }
+});
